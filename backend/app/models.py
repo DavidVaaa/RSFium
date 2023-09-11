9a8e160a4ffd2b9273ebecdb12f503d1bcac2212
David <dvazquez3@correo.um.edu.uy>
2023-09-11 11:49:42 -0300

    Initial commit

---
+++ backend/app/models.py
@@ -0,0 +1,37 @@
+# Create your models here.
+# backend/app/models.py
+
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser    
+
+
+class CustomUser(AbstractUser):
+    # Campos personalizados
+    rol = models.TextField
+    description = models.TextField(black=True)
+    phone_number = models.CharField(max_length=12, blank=True)
+    instagram = models.URLField(blank=True)
+    twitter = models.URLField(blank=True)
+    linkedin = models.URLField(blank=True)
+    # Otros campos como nombre, correo electrónico, contraseña, etc., ya están incluidos por defecto en AbstractUser
+
+class Student(CustomUser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rol = 'Student'
+
+class Staff(CustomUser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rol = 'Staff'
+
+class Teacher(CustomUser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rol = 'Teacher'
+
+class Admin(CustomUser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rol = 'Admin'
