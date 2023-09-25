ab47adec3f12f54b148ea35314f7933b59964efd
David <dvazquez3@correo.um.edu.uy>
2023-09-25 20:08:42 -0300

    ¿?*2

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -1,28 +1,38 @@
 from django.db import models
 
-class Materia(models.Model):
-    # Define the primary key for the model
+class Subject(models.Model):
     id_subject = models.IntegerField(primary_key=True)
-
-    # Define the name field for the model
     name_subject = models.CharField(max_length=255)
 
-    # Define the string representation for the model
     def __str__(self):
         return self.name_subject
 
-# class User(models.Model):
-#     id_user = models.IntegerField(primary_key=True)
-#     name_user = models.CharField()
+class User(models.Model):
+    id_user = models.IntegerField(primary_key=True)
+    name_user = models.CharField(max_length=255)
 
-#     # Define the string representation for the model
-#     def __str__(self):
-#         return self.name_user
+    def __str__(self):
+        return self.name_user
 
-# class Student(User):
-#     generation = models.IntegerField()
-#     orientation = models.CharField()
+class Student(User):
+    generation = models.IntegerField()
+    orientation = models.CharField(max_length=255)
 
-# class Crew(User):
-#     rol = models.
+class Crew(User):
+    pass
+
+class Teacher(User):
+    pass
+
+class Chat(models.Model):
+    id_chat = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    related_subject = models.ForeignKey(Materia, on_delete=models.SET_NULL, null=True)
+
+class Message(models.Model):
+    id = models.IntegerField(primary_key=True)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1000)
+    timestamp = models.DateTimeField()
 
--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from .models import Chat  
 
-def lista_de_chats(request):
-    chats = Chat.objects.all()  # Obtén todos los chats de la base de datos
-    return render(request, 'lista_chats.html', {'chats': chats})
-
-
+def chat_list(request):
+    chats = Chat.objects.all()
+    return render(request, 'backend/chat_list.html', {'chats': chats})
